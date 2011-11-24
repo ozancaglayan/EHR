93972c130c75c39e17b84587910e11717bf46a28
Ozan Çağlayan <ozan@pardus.org.tr>
2011-11-24 16:24:39 +0200

    Now it's like a real application thx to Gokmen

--- build.py
+++ build.py
@@ -6,3 +6,5 @@ import glob
 
 for ui in glob.glob1("ui", "*.ui"):
     os.system("/usr/bin/pyuic4 -o ui/ui_%s.py ui/%s -g ehr" % (ui.split(".")[0], ui))
+
+os.system("pyrcc4 data/icons.qrc -o icons_rc.py")
--- main.py
+++ main.py
@@ -4,4 +4,42 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-from ehr.main
+from ui.ui_main import Ui_EHRMainWindow
+from ui.ui_newrecord import Ui_NewRecordWindow
+
+class EHRNewRecordWindow(QtGui.QDialog, Ui_NewRecordWindow):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+
+        self.setupUi(self)
+
+        # Sİnyalleri filan burda bagla
+
+
+
+class EHR(QtGui.QDialog, Ui_EHRMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+
+        self.setupUi(self)
+
+        self.newRecordWindow = EHRNewRecordWindow(self)
+
+        self.pushButtonNewRecord.clicked.connect(self.newRecordWindow.show)
+        self.pushButtonExit.clicked.connect(QtGui.qApp.quit)
+
+        # self.pushButton2 ile erişiriz
+
+
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+
+    ehr = EHR()
+    ehr.show()
+
+    app.exec_()
